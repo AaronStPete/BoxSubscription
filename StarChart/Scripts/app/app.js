1fae617d0199730bbe97d18dcfc28580f1471748
AaronStPete <AaronStPete@users.noreply.github.com>
2018-05-08 18:33:31 -0400

    Ready to turn in

--- StarChart/Scripts/app/app.js
+++ StarChart/Scripts/app/app.js
@@ -12,6 +12,22 @@ app.config(function ($routeProvider) {
         controller: 'homeController'
     });
 
+    $routeProvider.when("/login", {
+        templateUrl: '/scripts/app/pages/login.html',
+        controller: 'homeController'
+    });
+
+    $routeProvider.when("/mgmtDashSubscription", {
+        templateUrl: '/scripts/app/pages/mgmtDashSubscriptions.html',
+        controller: 'homeController'
+    });
+
+    $routeProvider.when("/createAdmin", {
+        templateUrl: '/scripts/app/pages/createAdmin.html',
+        controller: 'homeController'
+    });
+
+
     $routeProvider.otherwise({ redirectTo: "/home" });
 });
 
